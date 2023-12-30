6250100231983c47a8afb70b176a3dc357bb73fd
gigabitamin <gigabitamin@gmail.com>
2023-12-30 10:16:30 +0900

    v0.2.2 231230

--- farm_quest_django/gardening_shop_app/urls.py
+++ farm_quest_django/gardening_shop_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('gardening_shop_index/', views.gardening_shop_index, name='gardening_shop_index'),      
+    path('gardening_shop_index/', views.gardening_shop_index, name='gardening_shop_index'),
+    path('gardening_shop_search/<str:keyword>/', views.gardening_shop_search, name='gardening_shop_search'),
 ]
