5f2f0ffe115947bcc816ced0b89e64d38e53363b
psh <shkb8995@gmail.com>
2024-01-23 15:39:31 +0900

    dsfs

--- farm_quest_react/src/App.js
+++ farm_quest_react/src/App.js
@@ -1,7 +1,4 @@
 import './App.css';
-<<<<<<< HEAD
-import Calender from './components/Calender.js';
-=======
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import SideNav from './pages/SideNav';
@@ -11,26 +8,23 @@ import './css/login.css';
 import './css/scheduler.css';
 import './css/style_ltj.css';
 import './css/community.css';
+// import Calender from './components/Calender.js';
 
 
->>>>>>> feather
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-        <Calender></Calender>
-=======
       <div className="header_wrap">        
       </div>
         <Header/>
         <SideNav/>
+        {/* <Calender></Calender> */}
 
         <Footer/>
         {/* <FooterJS/> */}
       <div className="footer_wrap">
       </div>
->>>>>>> feather
     </div>
   );
 }
