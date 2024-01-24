dcdc1f9d206bf09fefdb243c34d9fcc5993627cf
psh <shkb8995@gmail.com>
2024-01-24 09:32:30 +0900

    ff

--- farm_quest_react/src/App.js
+++ farm_quest_react/src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
-import './App.css';
-=======
 
->>>>>>> develop
 import Header from './pages/Header';
 import Footer from './pages/Footer';
 import SideNav from './pages/SideNav';
@@ -10,31 +6,14 @@ import Top from './pages/Top';
 import './css/common.css';
 import './css/style_kdy.css';
 import './css/login.css';
+import './css/scheduler.css';
 import './css/style_ltj.css';
 import './css/community.css';
-<<<<<<< HEAD
-// import Calender from './components/Calender.js';
-
-
-=======
 import './App.css';
->>>>>>> develop
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      <div className="header_wrap">        
-      </div>
-        <Header/>
-        <SideNav/>
-        {/* <Calender></Calender> */}
-
-        <Footer/>
-        {/* <FooterJS/> */}
-      <div className="footer_wrap">
-      </div>
-=======
       {/* <div className="header_wrap"></div> */}
         <Header/>
         <SideNav/>
@@ -42,7 +21,6 @@ function App() {
         <Footer/>
         {/* <FooterJS/> */}
       {/* <div className="footer_wrap"></div>       */}
->>>>>>> develop
     </div>
   );
 }
