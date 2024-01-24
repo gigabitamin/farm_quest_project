94f0952736b48ae565836951e68b1550ef85e1dc
gigabitamin <gigabitamin@gmail.com>
2024-01-24 10:37:22 +0900

    v0.4.1 240124_1030

--- farm_quest_react/src/pages/Top.js
+++ farm_quest_react/src/pages/Top.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import GardeningShopIndex from './gardeningshop/GardeningShopIndex';
+import GardeningShopDetail from './gardeningshop/GardeningShopDetail';
+
 import Community from "./community/Community";
 import UserRouter from "./user/UserRouter"
 import DiagnosisRouter from "./diagnosis/DiagnosisRoute"
@@ -16,11 +18,10 @@ const Top = () => {
     return (
         <div>            
             <Routes>
-                <Route path="/gardening_shop_index" element={<GardeningShopIndex currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} currentPage={currentPage} setCurrentPage={setCurrentPage} />} />
+                <Route path="/gardening_shop_index" element={<GardeningShopIndex currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} currentPage={currentPage} setCurrentPage={setCurrentPage} />} />            
+                <Route path="/gardening_shop_detail/:id" element={<GardeningShopDetail />} />
                 <Route path="/community/:mainType" element={<Community />} />
-
             </Routes>
-
             <UserRouter />
             <DiagnosisRouter />
             
