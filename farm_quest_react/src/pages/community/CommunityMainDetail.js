bdf9d3a3ad31bd4846801702057f594a63b7e236
S.H.Jang <jsh64566@gmail.com>
2024-01-26 17:45:27 +0900

    240126_1745 saving

--- farm_quest_react/src/pages/community/CommunityMainDetail.js
+++ farm_quest_react/src/pages/community/CommunityMainDetail.js
@@ -1,19 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Cookies, useCookies } from 'react-cookie';
+import { useCookies } from 'react-cookie';
+import CommunityMainComment from './CommunityMainComment';
 import axios from 'axios';
 
 const CommunityMainDetail = () => {
     const threadNo = useSelector(state => state.community.threadNo);
+    const initialForm = {cmt_content: '', thread_no: threadNo};
     const dispatch = useDispatch();
-    const [item, setItem] = useState({user:{}});
+    const [item, setItem] = useState({user:{}, thread_comments:[]});
+    const [form, setForm] = useState(initialForm);
     const [cookies] = useCookies(['id']);
-
 
     const loadContent = async () => {
         const response = await axios.get(`http://localhost:8000/community/detail/show/${threadNo}`);
         // 테스트 출력 
-        // console.log(response.data);
+        console.log(response.data);
         setItem(response.data);
     };
 
@@ -45,6 +47,29 @@ const CommunityMainDetail = () => {
         });
     };
 
+    const resetForm = () => {
+        setForm(initialForm)
+    };
+
+    const changeForm = (e) => {
+        let {name, value} = e.target;
+        // form의 정보를 그대로 가져오면서 키값이 name인 요소의 value 바꾸기
+        setForm({
+            ...form,
+            [name]: value 
+        });
+    };
+
+    const submitForm = async (event) => {
+        event.preventDefault();
+        await axios.post('http://localhost:8000/community/detail/comment/add/', form, {
+            headers: { Authorization: `Token  ${cookies.id}` }
+        });
+        const response = await axios.get(`http://localhost:8000/community/detail/show/${threadNo}`);
+        console.log(response.data)
+        setItem(response.data);
+    };
+
     useEffect(() => {
         loadContent();
     }, []);
@@ -54,22 +79,31 @@ const CommunityMainDetail = () => {
             <div className="community_detail_content_box">
                 {/* 게시물 클릭시 나오는 해당 게시물의 상세 내용 */}
                 <button onClick={backToMain}>뒤로가기</button>
-                <div className="content_detail_content_box_top">
-                    <div className="content_detail_content_box_user">{item.user.username}</div>
-                    <div className="content_detail_content_box_type">타입{item.thread_type}</div>
+                <div className="detail_content_box_top">
+                    <div className="detail_content_box_user">{item.user.nickname}</div>
+                    <div className="detail_content_box_type">타입{item.thread_type}</div>
                 </div>
-                <div className="content_detail_content_box_main">
-                    <div className="content_detail_content_box_title">{item.thread_title}</div>
-                    <div className="content_detail_content_box_content">{item.thread_content}</div>
+                <div className="detail_content_box_main">
+                    <div className="detail_content_box_title">{item.thread_title}</div>
+                    <div className="detail_content_box_content">{item.thread_content}</div>
                 </div>
                 <button onClick={toUpdate}>수정</button>
                 <button onClick={onDelete}>삭제</button>
             </div>
-            <div className="community_detail_comment_box">
-                
+            <div className="community_detail_comment_boxes">
+                {   
+                    item.thread_comments.map(commentItem => {
+                        return (
+                            <CommunityMainComment commentItem={commentItem} loadContent={loadContent} />
+                        );
+                    })
+                }
+                <form onReset={resetForm} onSubmit={submitForm}>
+                    <input type='text' name='cmt_content' onChange={changeForm} />
+                    <button type='submit'>등록</button>
+                </form>
             </div>
         </div>
-        
     );
 };
 
