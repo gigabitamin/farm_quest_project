b378e070602d6adb0b71a22c4bba7760dd379136
jsh <jsh64566@gmail.com>
2024-01-31 15:52:59 +0900

    240131_1552

--- farm_quest_django/diagnosis_app/modules/yolo_detection.py
+++ farm_quest_django/diagnosis_app/modules/yolo_detection.py
@@ -178,8 +178,10 @@ def detect(save_file_path, plant_name, user_select_plant):
         tf_predict_disease_list = []
         tf_predict_result_list_sorted = []
         crops_path_list = []
-        if len(serialized_boxes) >= 0:
+        if len(serialized_boxes) > 0:
+            print('이게 외않되?1')
             with tf.device('/physical_device:CPU:0'):
+                print('이게 외않되?2')
                 tf_predict_disease_list, crops_path_list = tf_detect(serialized_results_list, plant_name, user_select_plant, img_path)
                 print('please')
         
