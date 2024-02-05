e408b56062db752222faeee9024eb3b8521218d5
jsh <jsh64566@gmail.com>
2024-02-05 12:02:41 +0900

    240205_1202 jsh css

--- farm_quest_react/src/pages/community/CommunityRight.js
+++ farm_quest_react/src/pages/community/CommunityRight.js
@@ -3,8 +3,9 @@ import React from 'react';
 const CommunityRight = () => {
     return (
         <aside id="community_right_menu">
-            <div className="community_right_box">내 팜로그</div>
-            <div className="community_right_box">내 질문</div>
+            <div className="community_right_box">실시간인기글</div>
+            <div className="community_right_box">트렌드</div>
+            <div className="community_right_box">즐겨찾기</div>
         </aside>
     );
 };
