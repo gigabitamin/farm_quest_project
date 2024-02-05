2e351c795c098037ff921c5217b69f60bc746545
maiyaha <shkb8995@gmail.com>
2024-02-05 13:48:35 +0900

    fff

--- farm_quest_react/src/pages/community/CommunityRight.js
+++ farm_quest_react/src/pages/community/CommunityRight.js
@@ -3,9 +3,8 @@ import React from 'react';
 const CommunityRight = () => {
     return (
         <aside id="community_right_menu">
-            <div className="community_right_box">실시간인기글</div>
+            <div className="community_right_box">인기글</div>
             <div className="community_right_box">트렌드</div>
-            <div className="community_right_box">즐겨찾기</div>
         </aside>
     );
 };
