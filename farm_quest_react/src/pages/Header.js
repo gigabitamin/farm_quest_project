87f136d78b509e42321ce476509db45478378dde
gigabitamin <gigabitamin@gmail.com>
2024-02-06 22:12:44 +0900

    v0.7.9c kdy css fix 240206_2208

--- farm_quest_react/src/pages/Header.js
+++ farm_quest_react/src/pages/Header.js
@@ -90,7 +90,7 @@ const Header = () => {
                     <nav className="navbar_hd">
                         <ul>
 
-                            <DiagnosisLink />
+                            {/* <DiagnosisLink /> */}
                             {/* 네비게이션 드롭다운 수정, 최상단 className="navbar" 에 맞췄으니 수정시 주의 -kdy */}     
 
                             <div className="nav-item_hd">
