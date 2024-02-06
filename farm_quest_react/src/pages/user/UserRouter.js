1f7bd1a7a10a72b4e096c1cc04d1834a863fb0ac
gigabitamin <gigabitamin@gmail.com>
2024-02-07 02:01:52 +0900

    240207_0155

--- farm_quest_react/src/pages/user/UserRouter.js
+++ farm_quest_react/src/pages/user/UserRouter.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import Logout from './Logout';
 import Resister from './Resister';
 import Login from './Login';
 import { Routes, Route } from 'react-router-dom';
-import LoginValid from './LoginValid';
 import UserInfo from './UserInfo';
-import TestLink from './TestLink'
 import LoginCookie from './LoginCookie'
 import LoginCheck from './LoginCheck'
 import Profile from './Profile'
@@ -15,12 +12,9 @@ const UserRouter = () => {
         return (
 
         <Routes>                  
-            <Route path="/logout" element={<Logout />} />
-            <Route path="/login_valid" element={<LoginValid />} />
             <Route path="/resister" element={<Resister />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/user_info" element={<UserInfo />} />
-            <Route path="/test_link" element={<TestLink />} />
+            <Route path="/user_info" element={<UserInfo />} />            
             <Route path="/login_cookie" element={<LoginCookie />} />
             <Route path="/login_check" element={<LoginCheck />} />
             <Route path="/profile" element={<Profile />} />
