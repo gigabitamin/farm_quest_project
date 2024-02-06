5962f30039d2768f1cf5aba8ecdf38fd416fe238
jsh <jsh64566@gmail.com>
2024-02-06 22:03:01 +0900

    v0.7.9a 240206_2202 jsh

--- farm_quest_django/community_app/pagination.py
+++ farm_quest_django/community_app/pagination.py
@@ -2,7 +2,7 @@ from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 
 class CommunityPagination(PageNumberPagination):
-    page_size = 20
+    page_size = 5
 
     def get_paginated_response(self, data):
         return Response({
